5d737d7faaea2d040f05c3844ac924ce5cdaed60
Maxime Tournier <maxime.tournier@brain.riken.jp>
2014-11-18 22:42:16 +0900

    cbz !

---
+++ cbz.py
@@ -0,0 +1,65 @@
+"""
+take a picture directory and make a cbz from it
+"""
+
+import cv2
+import os
+import numpy as np
+import tempfile
+import subprocess
+
+
+def cbz( directory ):
+    # optimized for kobo glo
+    resolution = (1024, 768)
+
+    listdir = (os.path.join(directory, f) for f in os.listdir(directory))
+    files = (f for f in listdir if os.path.isfile(f))
+
+    # grayscale threshold, set large to keep color images
+    threshold = 10
+
+    tmpdir = tempfile.mkdtemp()
+    tmpfiles = []
+
+    try:
+        for f in files:
+            image = cv2.imread( f )
+            hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+            saturation = hsv[:, :, 1]
+            mean = np.mean(saturation)
+
+            grayscale = mean <= threshold
+
+            if grayscale:
+
+                height, width = image.shape[:2]
+
+                # rotate landscape
+                if width > height:
+                    image = np.rot90(image)
+                    height, width = image.shape[:2]
+
+                # scale
+                image = cv2.resize( image, resolution )
+
+                basename = os.path.split(f)[-1]
+
+                tmp = os.path.join(tmpdir, basename)
+                cv2.imwrite(tmp, image)
+                tmpfiles.append( tmp )
+
+        cbz = directory + '.cbz'
+        subprocess.call(['zip', '-9', '-j', cbz] + tmpfiles)
+
+    finally:
+        for f in tmpfiles:
+            os.remove( f )
+        os.rmdir( tmpdir )
+
+
+if __name__ == '__main__':
+    import sys
+
+    cbz( sys.argv[1] )
--- goodmanga.py
+++ goodmanga.py
@@ -8,13 +8,9 @@ import urllib2
 import urllib
 
 import re
-import sys
-
-manga = sys.argv[1]
-chapter = int(sys.argv[2])
-
 
 prefix = 'http://www.goodmanga.net/'
+
 
 def find(what, where):
     """find an attribute in a list"""
@@ -24,107 +20,157 @@ def find(what, where):
         return None
 
 
-class Parser(HTMLParser):
-    """
-    extract img/next links from html
-    """
-
-    def __init__(self):
-        HTMLParser.__init__(self)
-        self.stack = []
-
-        self.img = None
-        self.next = None
-        self.link = None
-        
-        
-    def handle_starttag(self, tag, attrs):
-        if tag == 'a':
-            
-            id = find('id', self.stack[-1][1])
-
-            if id and id == 'manga_viewer':
-                href = find('href', attrs)
-                self.next = href
-                self.link = (tag, attrs)
-
-        if tag == 'img':
-            if self.stack[-1] == self.link:
-                src = find('src', attrs)
-                self.img = src
-                
-        self.stack.append( (tag, attrs) )
-        
-    def handle_endtag(self, tag):
-        del self.stack[-1]
-        
-    def handle_data(self, data):
-        pass
+def get(manga, chapter):
 
 
-def fetch_infos(url):
-    """download and extract infos from url"""
+    class Parser(HTMLParser):
+        """
+        extract img/next links from html
+        """
+
+        def __init__(self):
+            HTMLParser.__init__(self)
+            self.stack = []
+
+            self.img = None
+            self.next = None
+            self.link = None
+
+
+        def handle_starttag(self, tag, attrs):
+            if tag == 'a':
+
+                id = find('id', self.stack[-1][1])
+
+                if id and id == 'manga_viewer':
+                    href = find('href', attrs)
+                    self.next = href
+                    self.link = (tag, attrs)
+
+            if tag == 'img':
+                if self.stack[-1] == self.link:
+                    src = find('src', attrs)
+                    self.img = src
+
+            self.stack.append( (tag, attrs) )
+
+        def handle_endtag(self, tag):
+            del self.stack[-1]
+
+        def handle_data(self, data):
+            pass
+
+
+    def infos(url):
+        """download and extract infos from url"""
+
+        response = urllib2.urlopen(url)
+        html = response.read()
+
+        parser = Parser()
+        parser.feed(html)
+
+        return parser
+
+    def download(url, filename):
+        """download picture"""
+
+        if not os.path.isfile(filename):
+            try:
+                print 'downloading', url
+                urllib.urlretrieve (url, filename)
+            except:
+                try:
+                    os.remove( filename )
+                except OSError:
+                    pass
+
+                raise
+        else:
+            print 'skipping', url, '({} exists)'.format(filename)
+
+    import os
+
+
+    def mkdir(manga, chapter):
+        """create output directories"""
+
+        outdir = manga + '/' + str(chapter)
+
+        try:
+            os.makedirs( outdir )
+        except OSError:
+            pass
+
+        return outdir
+
+
+    outdir = mkdir( manga, chapter )
+
+    first = prefix + manga + '/chapter/' + str(chapter)
+    url = first
+
+    while url:
+        i = infos(url)
+
+        basename = i.img.split('/')[-1].split('.')
+
+        head = basename[0].zfill(2)
+        tail = basename[1:]
+
+        filename = outdir + '/' + '.'.join([head] + tail)
+
+        download(i.img, filename)
+
+        url = i.next
+        if not re.match(first, url):
+            # end of chapter
+            break
+
+
+
+def chapters(manga):
     
+    class Parser(HTMLParser):
+        """
+        extract chapter list
+        """
+
+        def __init__(self):
+            HTMLParser.__init__(self)
+            self.select = None
+
+        def handle_starttag(self, tag, attrs):
+            if tag == 'select' and find('name', attrs) == 'chapter_select': 
+                self.select = (tag, attrs)
+                self.chapters = []
+                
+            elif tag == 'option' and self.select:
+                value = find('value', attrs)
+
+                chapter = int( value.split('/')[-1] )
+                self.chapters.append( chapter )
+
+                
+        def handle_endtag(self, tag):
+            if tag == 'select' and self.select:
+                self.select = None
+
+
+    # parse first chapter
+    url = prefix + manga + '/chapter/1'
     response = urllib2.urlopen(url)
     html = response.read()
 
     parser = Parser()
     parser.feed(html)
 
-    return parser
+    return parser.chapters
+                
+if __name__ == '__main__':
+    import sys
     
-def download(url, filename):
-    """download picture"""
+    manga = sys.argv[1]
+    chapter = int(sys.argv[2])
 
-    if not os.path.isfile(filename):
-        try:
-            print 'downloading', url
-            urllib.urlretrieve (url, filename)
-        except:
-            try:
-                os.remove( filename )
-            except OSError:
-                pass
-            
-            raise
-    else:
-        print 'skipping', url, '({} exists)'.format(filename)
-
-import os
-
-
-def mkdir(manga, chapter):
-    """create output directories"""
-    
-    outdir = manga + '/' + str(chapter)
-
-    try:
-        os.makedirs( outdir )
-    except OSError:
-        pass
-
-    return outdir
-
-
-outdir = mkdir( manga, chapter )
-
-first = prefix + manga + '/chapter/' + str(chapter)
-url = first
-
-while url:
-    infos = fetch_infos(url)
-
-    basename = infos.img.split('/')[-1].split('.')
-
-    head = basename[0].zfill(2)
-    tail = basename[1:]
-    
-    filename = outdir + '/' + '.'.join([head] + tail)
-
-    download(infos.img, filename)
-
-    url = infos.next
-    if not re.match(first, url):
-        # end of chapter
-        break
-    
+    get(manga, chapter)
